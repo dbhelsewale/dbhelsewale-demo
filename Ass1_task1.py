58fa0045458b603ca4ef071940f843d76d5f49be
dbhelsewale <dbhelsewale@gmail.com>
2026-02-06 19:05:29 +0530

    Add files via upload

---
+++ Ass1_task1.py
@@ -0,0 +1,22 @@
+# num1 = 5
+# num2 = 10
+
+num1 = int(input("Enter the 1st number: "))
+num2 = int(input("Enter the 2nd number: "))
+
+a = num1 + num2
+b = num1 - num2
+c = num1 * num2
+d = num1 / num2
+
+print("Addition: ", a)
+print("subtraction: ", b)
+print("Multiplication: ", c)
+print("Division: ", d)
+
+
+
+
+
+
+
---
+++ Ass1_task2.py
@@ -0,0 +1,11 @@
+# fname = "John"
+# lname = "Doe"
+
+fname = input("Enter your first name: ")
+lname = input("Enter your last name: ")
+full_name = fname + " " + lname
+
+print("Enter your first name:", fname)
+print("Enter your last name:", lname)
+
+print("Hello, " + full_name + "! Welcome to the Python program")
